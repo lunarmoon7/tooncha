0c6b44621c50f5ead708b346b07c4f3487940eae
lunarmoon7 <49crehbgr@gmail.com>
2023-08-23 14:25:26 +0900

    init: 프로젝트 초기화

---
+++ android/app/src/main/kotlin/com/example/tooncha/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tooncha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+// Starting Point of Our App
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
