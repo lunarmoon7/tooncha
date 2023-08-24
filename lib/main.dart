7af37d9c19273892bf95246b4d053f9aa0b7c2f5
lunarmoon7 <49crehbgr@gmail.com>
2023-08-24 16:48:09 +0900

    feat: Icons and Transforms

--- lib/main.dart
+++ lib/main.dart
@@ -111,50 +111,63 @@ class App extends StatelessWidget {
                   height: 20,
                 ),
                 Container(
+                  clipBehavior: Clip.hardEdge,
                   decoration: BoxDecoration(
                     color: const Color(0xFF1f2123),
                     borderRadius: BorderRadius.circular(20),
                   ),
                   child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          const Text('Euro',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.w600)),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              const Text(
-                                '6 428',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                ),
-                              ),
+                              const Text('Euro',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 32,
+                                      fontWeight: FontWeight.w600)),
                               const SizedBox(
-                                width: 5,
+                                height: 10,
                               ),
-                              Text(
-                                "EUR",
-                                style: TextStyle(
-                                  color: Colors.white.withOpacity(0.8),
-                                  fontSize: 20,
-                                ),
+                              Row(
+                                children: [
+                                  const Text(
+                                    '6 428',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    "EUR",
+                                    style: TextStyle(
+                                      color: Colors.white.withOpacity(0.8),
+                                      fontSize: 20,
+                                    ),
+                                  )
+                                ],
                               )
                             ],
-                          )
+                          ),
+                          Transform.scale(
+                              scale: 2.2,
+                              child: Transform.translate(
+                                offset: const Offset(-5, 12),
+                                child: const Icon(
+                                  Icons.euro_rounded,
+                                  color: Colors.white,
+                                  size: 98,
+                                ),
+                              ))
                         ],
-                      ),
-                    ]),
-                  ),
-                )
+                      )),
+                ),
               ],
             )),
       ),
